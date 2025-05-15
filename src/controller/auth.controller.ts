f0a2f995e0cdb43c31aca066eea2a84d4ea39093
theridwanade <royeniyiadegboyega@gmail.com>
2025-05-15 11:56:47 +0100

    implement user signup functionality with password hashing and response handling

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import User from "../model/users.model";
 
 export const authSignupController = async (req: Request, res: Response) => {
   try {
@@ -11,6 +13,36 @@ export const authSignupController = async (req: Request, res: Response) => {
       });
       return;
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    let newUser;
+    if(firstname && lastname) {
+      newUser = new User({
+        email,
+        password: hashedPassword,
+        firstname,
+        lastname,
+      });
+    } else {
+      newUser = new User({
+        email,
+        password: hashedPassword,
+      });
+    }
+    const user = await newUser.save();
+    if (!user) {
+      res.status(500).json({
+        message: "User not created",
+        code: 500,
+        success: false,
+      });
+      return;
+    }
+    res.status(201).json({
+      message: "User created successfully",
+      code: 201,
+      success: true,
+    });
   } catch (error: any) {
     res.status(500).json({
       message: error.message,
