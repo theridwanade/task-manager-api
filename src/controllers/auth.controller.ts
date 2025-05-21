d5df01dc9d53a3ba664378f4a1c7e3acd604b14a
theridwanade <royeniyiadegboyega@gmail.com>
2025-05-21 14:39:36 +0100

    add refresh token functionality with cookie handling and update auth routes, remain updating authorization header

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { authLoginService, authSignupService } from "../services/auth.service";
+import { generateToken, verifyToken } from "../util/jwttoken";
+import User from "../model/users.model";
 
 export const authSignupController = async (req: Request, res: Response) => {
   try {
@@ -32,12 +34,15 @@ export const authLoginController = async (req: Request, res: Response) => {
       secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
     });
-    res.status(result.code).set("Authorization", `Bearer ${result.data!.token}`).json({
-      message: result.message,
-      code: result.code,
-      success: result.success,
-      data: result.data,
-    });
+    res
+      .status(result.code)
+      .set("Authorization", `Bearer ${result.data!.token}`)
+      .json({
+        message: result.message,
+        code: result.code,
+        success: result.success,
+        data: result.data,
+      });
   } catch (error: any) {
     res.status(500).json({
       message: error.message,
@@ -46,8 +51,7 @@ export const authLoginController = async (req: Request, res: Response) => {
     });
     return;
   }
-}
-
+};
 
 export const authLogoutController = async (req: Request, res: Response) => {
   try {
@@ -57,6 +61,79 @@ export const authLogoutController = async (req: Request, res: Response) => {
       code: 200,
       success: true,
     });
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+      code: 500,
+      success: false,
+    });
+    return;
+  }
+};
+
+export const authRefreshTokenController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { refreshToken } = req.cookies;
+    if (!refreshToken) {
+      res.status(401).json({
+        message: "Refresh token not found",
+        code: 401,
+        success: false,
+      });
+      return;
+    }
+    const decodeRefreshToken = await verifyToken(refreshToken);
+    if (!decodeRefreshToken) {
+      res.status(401).json({
+        message: "Invalid refresh token",
+        code: 401,
+        success: false,
+      });
+      return;
+    }
+    const user = await User.findById(decodeRefreshToken.userId);
+    if (!user) {
+      res.status(401).json({
+        message: "User not found",
+        code: 401,
+        success: false,
+      });
+      return;
+    }
+    if (!user.isVerified) {
+      res.status(401).json({
+        message: "User not verified",
+        code: 401,
+        success: false,
+      });
+      return;
+    }
+    const token = await generateToken(
+      {
+        userId: user._id,
+        email: user.email,
+        firstname: user.firstname,
+        lastname: user.lastname,
+      },
+      60 * 60
+    ); // 1 hour
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    });
+    res.status(200).json({
+      message: "Token refreshed successfully",
+      code: 200,
+      success: true,
+      data: {
+        token,
+      },
+    });
+    return;
   } catch (error: any) {
     res.status(500).json({
       message: error.message,
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { authSignupController, authLoginController, authLogoutController } from "../controllers/auth.controller";
+import { authSignupController, authLoginController, authLogoutController, authRefreshTokenController } from "../controllers/auth.controller";
 
 const routes = Router();
 
 routes.post("/signup", authSignupController);
 routes.post("/login", authLoginController);
 routes.get("/logout", authLogoutController);
+routes.get("/refresh-token", authRefreshTokenController);
 
 const authRoutes = routes;
 export default authRoutes;
